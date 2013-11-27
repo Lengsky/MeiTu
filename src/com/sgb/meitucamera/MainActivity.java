8e002b988d3f5616fd0f614578b675c17e94bdf5
hhhhh <hhhhh@lenovo-PC>
2013-11-27 17:03:47 +0800

    测试

--- src/com/sgb/meitucamera/MainActivity.java
+++ src/com/sgb/meitucamera/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
 
+	private int MkkA;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
